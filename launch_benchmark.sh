e42ba707fc1aa54d7d73e18342cabd66b3f5878f
zxd1997066 <1136902113@qq.com>
2023-05-05 08:53:34 +0000

    debug

--- launch_benchmark.sh
+++ launch_benchmark.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -xe
 
+model_name=$(echo $@ |sed 's/.*--model_name.//;s/ *--.*//')
+
 if [ "${model_name}" == "deepspeech" ];then
-    echo $@
     ./launch_benchmark_deepspeech.sh $@
 elif [ "${model_name}" == "NCF" ];then
     ./launch_benchmark_ncf.sh $@
