7d2c3339289ea0d076d2e7d72955d81d015c5eaa
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2024-03-16 00:13:10 +0800

    Update launch_benchmark.sh

--- launch_benchmark.sh
+++ launch_benchmark.sh
@@ -43,7 +43,7 @@ function main {
         for batch_size in ${batch_size_list[@]}
         do
             if [ $batch_size -le 0 ];then
-                batch_size=64
+                batch_size=16
             fi
             # clean workspace
             logs_path_clean
